dd7705d1f139237142eaef7ff77b2e9a7e11ccd9
Raja08-kee <rajasekharwiz@gmail.com>
2026-05-06 14:23:55 +0530

    fixed backend url

--- src/pages/ContactForm.jsx
+++ src/pages/ContactForm.jsx
@@ -25,7 +25,7 @@ const handleSubmit = async (e) => {
     console.log("📤 Sending Data:", formData);
 
     try {
-        const response = await fetch("http://localhost:5000/api/contact", {
+        const response = await fetch("https://appscare-redesign-1.onrender.com/api/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
